890e4727e242f394336dc82cf93fda79336b1850
Jakub Perdoch <65115839+Etu14@users.noreply.github.com>
2023-02-19 21:54:57 +0100

    Skratenie kodu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,34 +15,32 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game !🥲';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess === randomNumber) {
-    document.querySelector('.number').textContent = randomNumber;
-    document.querySelector('.message').textContent = 'Correct Number! 🎉';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '20vw';
-  } else if (guess > randomNumber) {
+  } else if (guess !== randomNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high! 📈';
+      document.querySelector('.message').textContent =
+        guess > randomNumber ? 'Too high! 📈' : 'Too low! 📉';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'You lost the game! 🥲';
       document.querySelector('.score').textContent = 0;
     }
-  } else {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low! 📉';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game 🥲';
-      document.querySelector('.score').textContent = 0;
+  } else if (guess === randomNumber) {
+    document.querySelector('.number').textContent = randomNumber;
+    document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '20vw';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
+  document.querySelector('.score').textContent = score;
   randomNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.message').textContent = 'Start guessing...';
