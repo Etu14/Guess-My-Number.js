8de85a0203db9815a8ea0283e30d48a48ee10ff4
Jakub Perdoch <65115839+Etu14@users.noreply.github.com>
2023-02-19 20:55:40 +0100

    Reset

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = randomNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -16,6 +15,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess === randomNumber) {
+    document.querySelector('.number').textContent = randomNumber;
     document.querySelector('.message').textContent = 'Correct Number! 🎉';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '20vw';
@@ -38,4 +38,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').style.width = '12vw';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
 });
